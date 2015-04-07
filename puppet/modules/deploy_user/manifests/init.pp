38c8fcd1818400ad9e4f14c0a10a6e96a7e7075a
Eugene Westbrook <eugenewestbrook@gmail.com>
2015-04-06 22:10:28 -0400

    Gives deploy user sudo

--- puppet/modules/deploy_user/manifests/init.pp
+++ puppet/modules/deploy_user/manifests/init.pp
@@ -1,4 +1,14 @@
 class deploy_user {
+
+  file { '/etc/sudoers.d/deploy':
+    content => '%deploy ALL = (ALL) NOPASSWD: ALL',
+    ensure => 'file',
+    owner => 'root',
+    group => 'root',
+    mode => '750',
+    require => Group['deploy']
+  }
+
   group { 'deploy':
     ensure => 'present'
   }
