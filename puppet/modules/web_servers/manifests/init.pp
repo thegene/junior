e80c3ee7ab9c0f16488fc35c63cc7d6811057ad7
Eugene Westbrook <eugenewestbrook@gmail.com>
2015-04-25 16:59:59 -0400

    Sets up nginx with wedding site but no content

--- puppet/modules/web_servers/manifests/init.pp
+++ puppet/modules/web_servers/manifests/init.pp
@@ -1,15 +1,41 @@
 class web_servers($domain = 'localhost') {
-  include www
+
+  package { 'nginx':
+    ensure => '1.0.15-11.el6',
+    require => File['/var/www/apps']
+  }
+
+  service { 'nginx':
+    ensure => 'running',
+    require => Package['nginx']
+  }
+
+  file { ['/etc/nginx/conf.d/default.conf',
+          '/etc/nginx/conf.d/ssl.conf',
+          '/etc/nginx/conf.d/virtual.conf']:
+    ensure => 'absent',
+    require => Package['nginx']
+  }
+
+  file { ['/var/www', '/var/www/apps']:
+    ensure => 'directory',
+    owner => 'deploy',
+    group => 'deploy',
+    mode => '755',
+    require => User['deploy']
+  }
 
   web_servers::server { 'wedding':
-    app_name => 'wedding'
+    app_name => 'wedding',
+    domain => $domain,
+    notify => Service['nginx']
   }
 
 }
 
-define web_servers::server($app_name) {
-  $app_root = "/var/www/apps/${app_name}"
-
+define web_servers::server($app_name, $domain) {
+  $fqdn = "${app_name}.${domain}"
+  $app_root = "/var/www/apps/${fqdn}"
 
   file { "${app_name}_directory":
     path => $app_root,
@@ -19,7 +45,17 @@ define web_servers::server($app_name) {
     mode => '755',
   }
 
-  nginx::resource::vhost { "${app_name}.${web_servers::domain}":
-    www_root => "${app_root}/current",
+  file { "${app_name}-host":
+    path => "/etc/nginx/conf.d/${app_name}.conf",
+    content => template('web_servers/host.erb'),
+    mode => '755',
+    owner => 'nginx'
+  }
+
+  file { "/var/log/nginx/${fqdn}":
+    ensure => 'directory',
+    mode => '755',
+    owner => 'nginx',
+    group => 'nginx'
   }
 }
